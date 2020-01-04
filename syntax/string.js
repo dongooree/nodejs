ea0d256f711bbbcb3d0b5455fecd318642169758
Dongeun <pde0406@gmail.com>
2020-01-04 23:51:28 +0900

    chapter 1-24

---
+++ syntax/string.js
@@ -0,0 +1,2 @@
+console.log('1'+'1');   //string이므로 결합 연산자가 됨
+console.log('alsfejslai asliejsf alsiefj  asliejfsalafsiejf alsiefjlaisejf'.length);
---
+++ syntax/template.js
@@ -0,0 +1,11 @@
+var name = 'Dongeun';
+var letter = 'Dear ' + name + '\n\
+\
+asegasio oasijf soisjeofisjoefs soiefjso fosiejf sadg gkdghrkda waoivkxjvn sivdoi v sfsoiejf. ' + name;
+
+var letter = `Dear ${name}
+${1+1}
+asegasio oasijf soisjeofisjoefs soiefjso fosiejf sadg gkdghrkda waoivkxjvn sivdoi v sfsoiejf. ${name}`;
+
+console.log(letter);
+
